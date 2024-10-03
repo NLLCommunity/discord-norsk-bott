f91f4c425566c6f762255ef75add3f94c4750e24
Vegard Berg <mail@myrkvi.com>
2024-10-03 17:32:07 +0200

    chore: Disable music link event listener

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,11 @@ import EventEmitter from 'events';
   ],
   providers: NestClassCollection.fromInjectables(providers)
     .concat(NestClassCollection.fromInjectables(commands))
-    .concat(NestClassCollection.fromInjectables(handlers))
+    .concat(
+      NestClassCollection.fromInjectables(handlers).except(
+        handlers.MusicLinkHandler,
+      ),
+    )
     .toArray(),
 })
 export class AppModule {}
